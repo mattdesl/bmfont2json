69d207073ba406c61382bb00eff9ccf4d0c01093
j-waechter <jonas-waechter@t-online.de>
2016-02-08 00:50:21 +0100

    Added --image-file-extension switch

--- bin/bmfont2json.js
+++ bin/bmfont2json.js
@@ -1,16 +1,41 @@
 #!/usr/bin/env node
 
+// Define command line usage
 var argv = require('yargs')
-				.usage('Usage: file [options]')
-				.alias('o', 'output')
-				.alias('p', 'pretty')
-				.describe('o', 'the output path')
-				.describe('p', 'pretty-print the JSON output')
-				.demand(1)
-				.argv;
+		.usage('Usage: file [options]')
+		.demand(1)
+		.help('help').alias('help', 'h')
+		.options({
+			'output': {
+				alias: 'o',
+				describe: 'The output path',
+				requiresArg: true,
+				string: true
+			},
+			'image-file-extension': {
+				alias: 'x',
+				describe: 'Change the extension of the image file names to the provided one',
+				requiresArg: true,
+				string: true
+			},
+			'pretty': {
+				alias: 'p',
+				describe: 'Pretty-print the JSON output'
+			}
+		})
+		.argv;
 
 var parse = require('../');
 var fs = require('fs');
+
+// Validate image file extension
+if (argv.imageFileExtension) {
+	var newExtension = argv.imageFileExtension.replace(/^\./, '');
+	if (!/^\w+$/.test(newExtension)) {
+		throw 'Invalid image file extension: ' + argv.imageFileExtension;
+	}
+	argv.imageFileExtension = newExtension;
+}
 
 fs.readFile( argv._[0], function(err, data) {
 	if (err)
@@ -18,8 +43,15 @@ fs.readFile( argv._[0], function(err, data) {
 
 	var obj = parse(data);
 
+	if (argv.imageFileExtension) {
+		var pages = obj.pages;
+		for (var i = 0; i < pages.length; i++) {
+			pages[i] = pages[i].replace(/(.\.)\w+$/, '$1' + argv.imageFileExtension);
+		}
+	}
+
 	var jsonOut = JSON.stringify(obj, undefined, argv.p ? 2 : undefined);
 
 	var writer = argv.o ? fs.createWriteStream(argv.o) : process.stdout;
 	writer.write(jsonOut+ (argv.p?'\n':''));
-})
+});
